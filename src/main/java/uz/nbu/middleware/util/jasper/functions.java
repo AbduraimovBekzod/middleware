30ced9acc6e2f502fc062ee3cf88b14503143c41
BekzodAbduraimov <bekandshika@gmail.com>
2020-11-25 16:49:13 +0500

    delimiter (in linux ',')

--- src/main/java/uz/nbu/middleware/util/jasper/functions.java
+++ src/main/java/uz/nbu/middleware/util/jasper/functions.java
@@ -5,7 +5,7 @@ public class Functions {
 
         number = number.replaceAll(" ", "").replaceAll(",", ".");
         return number.equals("") ? "":
-                String.format("%,.2f", Double.parseDouble(number));
+                String.format("%,.2f", Double.parseDouble(number)).replaceAll(",", " ");
 //        String.format("%,.2f", Double.parseDouble(number)).replaceAll(",", " ");
     }
 }
