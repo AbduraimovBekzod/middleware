0bd5eb20e5e9efc4879136cfa186a7cde0b9d909
AbduraimovBekzod <bekandshika@gmail.com>
2021-02-22 12:19:16 +0500

    add ADDRESS to SCORING_RESULT

--- src/main/java/uz/nbu/middleware/util/jasper/ScoringResults.java
+++ src/main/java/uz/nbu/middleware/util/jasper/ScoringResults.java
@@ -19,30 +19,26 @@ public class ScoringResults {
                 JasperCompileManager.compileReport(dir + "reports/scoring_results/uzb/scoring_results.jrxml");
 
         Functions func = new Functions();
+        requestObj.put("result_sum", func.indents(requestObj.getString("result_sum")));
 
-        Map<String, Object> parameters = new HashMap<String, Object>();
+        String str = requestObj.toString();
 
-        parameters.put("result_emp_fio", requestObj.getString("result_emp_fio"));
-        parameters.put("result_borrower_fio", requestObj.getString("result_borrower_fio"));
-        parameters.put("result_borrower_phone", requestObj.getString("result_borrower_phone"));
-        parameters.put("result_borrower_address", requestObj.getString("result_borrower_address"));
-        parameters.put("result_borrower_date", requestObj.getString("result_borrower_date"));
-        parameters.put("result_borrower_request_number", requestObj.getString("result_borrower_request_number"));
-        parameters.put("result_product", requestObj.getString("result_product"));
-        parameters.put("result_sum", func.indents(requestObj.getString("result_sum")));
-        parameters.put("result_currency", requestObj.getString("result_currency"));
-        parameters.put("result_filial", requestObj.getString("result_filial"));
+        try (ByteArrayInputStream is = new ByteArrayInputStream(str.getBytes())) {
+            Map<String, Object> parametersMap = new HashMap<>();
 
-        JRDataSource dataSource = new JREmptyDataSource();
-        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, dataSource);
-        JasperExportManager.exportReportToPdfFile(jasperPrint, dir + "output/scoring_results.pdf");//create pdf file
+            parametersMap.put(JsonQueryExecuterFactory.JSON_INPUT_STREAM, is);
+            JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parametersMap);
+            JasperExportManager.exportReportToPdfFile(jasperPrint, "output" + File.separator + "scoring_results.pdf");
+        }
 
-        File file = new File(dir + "output/scoring_results.pdf");
+        File file = new File("output" + File.separator + "scoring_results.pdf");
+
         FileInputStream fis = new FileInputStream(file);
 
         ByteArrayOutputStream bos = new ByteArrayOutputStream(); //BLOB
         byte[] buf = new byte[1024];
-        for (int readNum; (readNum = fis.read(buf)) != -1; ) {
+        for(int readNum; (readNum = fis.read(buf)) != -1;)
+        {
             bos.write(buf, 0, readNum);
         }
 
